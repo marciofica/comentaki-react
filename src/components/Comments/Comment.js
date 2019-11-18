2a6416fc1c0393a2e878258ecf3a5946df22dee8
marciofica@gmail.com <marciofica@gmail.com>
2019-11-18 07:39:29 -0300

    Ajuste visualização

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -3,8 +3,11 @@ import Time from '../Time'
 
 const Comment = ({comment}) => {
     return (
-        <div>
-            {comment.content} por <i>{comment.user.name}</i> em: <Time timestamp={comment.createdAt} />
+        <div className='p-1'>
+            <blockquote className='blockquote'>
+                <p className='mb-0'>{comment.content}</p>
+                <footer className='blockquote-footer'>Comentado por: <cite title='Comentado por'>{comment.user.name} em <Time timestamp={comment.createdAt} /> </cite></footer>
+            </blockquote>
         </div>
     )
 }
